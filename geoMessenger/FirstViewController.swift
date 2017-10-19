906283766719022806a2db226138f717bcca0ee3
JTzBallin515 <jeremy.rodriguez@marquette.edu>
2017-10-18 22:40:53 -0500

    Initial Commit

---
+++ geoMessenger/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  geoMessenger
+//
+//  Created by Jeremy Rodriguez on 10/18/17.
+//  Copyright © 2017 Jeremy Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
